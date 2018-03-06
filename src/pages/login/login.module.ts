de9beab68a480e6b142d5914bf848f17217535b6
GaetanMSI <gmirmand@gmail.com>
2018-03-06 21:24:05 +0100

    first commit

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,19 +1,25 @@
-import { NgModule } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { IonicPageModule } from 'ionic-angular';
+import {NgModule} from '@angular/core';
+import {TranslateModule} from '@ngx-translate/core';
+import {IonicPageModule} from 'ionic-angular';
 
-import { LoginPage } from './login';
+import {IonSimpleWizard} from '../ion-simple-wizard/ion-simple-wizard.component';
+import {IonSimpleWizardStep} from '../ion-simple-wizard/ion-simple-wizard.step.component';
+
+import {LoginPage} from './login';
 
 @NgModule({
-  declarations: [
-    LoginPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(LoginPage),
-    TranslateModule.forChild()
-  ],
-  exports: [
-    LoginPage
-  ]
+    declarations: [
+        LoginPage,
+        IonSimpleWizard,
+        IonSimpleWizardStep
+    ],
+    imports: [
+        IonicPageModule.forChild(LoginPage),
+        TranslateModule.forChild()
+    ],
+    exports: [
+        LoginPage
+    ]
 })
-export class LoginPageModule { }
+export class LoginPageModule {
+}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,50 +1,68 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {IonicPage, NavController, ToastController, Events} from 'ionic-angular';
 
-import { User } from '../../providers/providers';
-import { MainPage } from '../pages';
+import {User} from '../../providers/providers';
+import {MainPage} from '../pages';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+    selector: 'page-login',
+    templateUrl: 'login.html'
 })
 export class LoginPage {
-  // The account fields for the login form.
-  // If you're using the username field with or without email, make
-  // sure to add it to the type
-  account: { email: string, password: string } = {
-    email: 'test@example.com',
-    password: 'test'
-  };
+    // The account fields for the login form.
+    // If you're using the username field with or without email, make
+    // sure to add it to the type
+    account: { email: string, password: string } = {
+        email: 'test@example.com',
+        password: 'test'
+    };
 
-  // Our translated text strings
-  private loginErrorString: string;
+    // Our translated text strings
+    private loginErrorString: string;
 
-  constructor(public navCtrl: NavController,
-    public user: User,
-    public toastCtrl: ToastController,
-    public translateService: TranslateService) {
+    step: any;
+    stepCondition: any;
+    stepDefaultCondition: any;
+    currentStep: any;
 
-    this.translateService.get('LOGIN_ERROR').subscribe((value) => {
-      this.loginErrorString = value;
-    })
-  }
+    constructor(public navCtrl: NavController,
+                public user: User,
+                public toastCtrl: ToastController,
+                public translateService: TranslateService,
+                public evts: Events) {
 
-  // Attempt to login in through our User service
-  doLogin() {
-    this.user.login(this.account).subscribe((resp) => {
-      this.navCtrl.push(MainPage);
-    }, (err) => {
-      this.navCtrl.push(MainPage);
-      // Unable to log in
-      let toast = this.toastCtrl.create({
-        message: this.loginErrorString,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
-    });
-  }
+        this.translateService.get('LOGIN_ERROR').subscribe((value) => {
+            this.loginErrorString = value;
+        });
+
+        /**
+         * Step Wizard Settings
+         */
+        this.step = 1;//The value of the first step, always 1
+        this.stepCondition = false;//Set to true if you don't need condition in every step
+        this.stepDefaultCondition = this.stepCondition;//Save the default condition for every step
+    }
+
+    //Wizard
+    toggle() {
+        this.stepCondition = !this.stepCondition;
+    }
+
+    // Attempt to login in through our User service
+    doLogin() {
+        this.user.login(this.account).subscribe((resp) => {
+            this.navCtrl.push(MainPage);
+        }, (err) => {
+            this.navCtrl.push(MainPage);
+            // Unable to log in
+            let toast = this.toastCtrl.create({
+                message: this.loginErrorString,
+                duration: 3000,
+                position: 'top'
+            });
+            toast.present();
+        });
+    }
 }
